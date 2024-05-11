3ae2fa996c327e8b1f1938c2e04a46f0bc68273d
adithya1770 <adithyaps929@gmail.com>
2024-05-12 00:25:43 +0530

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,22 @@ function App() {
   
     <center>
       <div className='lg:mt-0 sm:mt-16'><br />
-        <a href="/projects" className='block text-2xl text-center mb-4 sm:inline-block hover:text-3xl shadow0 sm:mr-4 hover:text-gray-700 transition duration-100 ease-in-out'>PROJECTS</a>
-        <a href="/info" className='block text-2xl text-center mb-4 lg:pl-20 sm:inline-block hover:text-3xl shadow0 sm:mr-4 hover:text-gray-700 transition duration-100 ease-in-out'>KNOW ME</a>
-        <a href="/contact" className='block text-2xl text-center hover:text-gray-700 hover:text-3xl shadow0 transition duration-100 ease-in-out'>GET IN TOUCH</a>
+        <a href="/projects" className='block text-2xl text-center mb-4 sm:inline-block hover:text-3xl shadow0 sm:mr-4 hover:text-white transition duration-100 ease-in-out h-10 w-44 hover:backdrop-blur-lg rounded-xl bg-pixel'>PROJECTS</a>
+        <a href="/info" className='block text-2xl text-center mb-4 lg:pl-15 sm:inline-block hover:text-3xl shadow0 sm:mr-4 hover:text-white transition duration-100 ease-in-out h-10 w-48 hover:backdrop-blur-lg rounded-xl bg-pixelred'>KNOW ME</a>
+        <a href="/contact" className='block text-2xl text-center hover:text-white hover:text-3xl shadow0 transition duration-100 ease-in-out h-10 w-44 hover:backdrop-blur-lg rounded-xl bg-pixelgreen'>CONTACT</a>
       </div>
     </center>
     <br />
     <div className='lg:mt-52 lg:mr-80 text-center text-4xl'>
-    <h1 className='lg:text-3xl sm:text-5xl font-bold hover:text-6xl'>(ʕ •-•ʔ )</h1>
-      <h1 className='text-3xl sm:text-6xl md:text-5xl lg:text-8xl font-bold'>Hello!</h1>
-      <h3 className='text-xl sm:text-4xl md:text-3xl lg:text-6xl font-semibold'>It's me <i><b>Adithya</b></i></h3>
+    <h1 className='lg:text-3xl sm:text-4xl font-bold hover:text-5xl'>(ʕ •-•ʔ )</h1>
+      <h1 className='text-3xl sm:text-8xl md:text-5xl lg:text-8xl font-bold '>Hello!</h1>
+      <h3 className='text-xl sm:text-4xl md:text-3xl lg:text-6xl font-semibold'>It's me <i><b>Adithya!!</b></i></h3>
     </div>
     
-    <div className='absolute bottom-0 w-full text-center'>
-      <p className='text-lg sm:text-xl lg:text-2xl inline-block mr-10'><a href="https://github.com/adithya1770/adithya1770">Github</a></p>
-      <p className='text-lg sm:text-xl lg:text-2xl inline-block'>2024AdithyaPS</p>
+    <div className='absolute bottom-0  text-center h-10 w-full bg-black text-white'>
+      <p className='text-lg sm:text-xl lg:text-2xl inline-block mr-5'><a href="https://github.com/adithya1770/adithya1770">Github </a></p>
+      <p className='text-lg sm:text-xl lg:text-2xl inline-block pl-5'>2024</p>
+      <p className='text-lg sm:text-xl lg:text-2xl inline-block pl-10'>AdithyaPS&copy;</p>
     </div>
 
     <Routes>
@@ -35,7 +36,7 @@ function App() {
       <Route path="/projects" element={<Projects />} />
     </Routes>
     
-      <div className='h-36 w-32 bg-white absolute lg:top-12 rounded-full lg:left-10 left-28 bottom-12'>
+      <div className='h-36 w-32 bg-white absolute lg:top-12 rounded-full lg:left-10 left-28 bottom-12 shadow-2xl shadow-black sha'>
         <img src="./assets/comp.png" className='h-36 w-32 rounded-full'/>
       </div>
   </div>
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,10 +1,10 @@
 function Projects(){
     return(
-        <div className="lg:flex lg:w-96 lg:h-96 absolute lg:top-44 lg:right-44 font-semibold lg:text-3xl lg:text-justify h-28 w-64 top-80 right-12">
+        <div className="lg:flex lg:w-96 lg:h-96 absolute lg:top-44 lg:right-44 font-semibold lg:text-3xl lg:text-justify h-28 w-64 bottom-80 right-12">
             <ul>
-                <li><b><a href="https://github.com/adithya1770/To-N-Do">To'N'Do</a></b> : A todo app made using ExpressJS</li>
-                <li><b><a href="https://github.com/adithya1770/sceptix-webapp">OpenMath</a></b> : A Matrix calculator and Math Website</li>
-                <li><b><a href="https://github.com/adithya1770/learn-react">Farmy</a></b> : Web portal for famers(Beta)</li>
+                <li><b><a href="https://github.com/adithya1770/To-N-Do" className="underline">To'N'Do</a></b> : A todo app made using ExpressJS</li>
+                <li><b><a href="https://github.com/adithya1770/sceptix-webapp" className="underline">OpenMath</a></b> : A Matrix calculator and Math Website</li>
+                <li><b><a href="https://github.com/adithya1770/learn-react" className="underline">Farmy</a></b> : Web portal for famers(Beta)</li>
                 <li><b>SaveLyf</b> : Alcohol Detector using RaspberryPI</li>
             </ul>
         </div>
