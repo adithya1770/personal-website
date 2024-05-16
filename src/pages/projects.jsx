57faffd5a5d3d7a27eb984a65518744ca5dc0db8
adithya1770 <adithyaps929@gmail.com>
2024-05-16 22:47:24 +0530

    projects section ui change

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,12 +1,12 @@
 import "./styler.css"
 function Projects(){
     return(
-        <div className="lg:flex lg:w-96 lg:h-96 absolute lg:top-44 lg:right-44 font-semibold lg:text-xl lg:text-justify h-52 w-72 top-96 right-8 overflow-y-scroll custom-scrollbar">
+        <div className="lg:flex lg:w-96 lg:h-96 absolute lg:top-44 lg:right-44 font-semibold lg:text-xl lg:text-justify h-52 w-80 top-96 right-8 overflow-y-scroll custom-scrollbar rounded-2xl">
             <ul>
-                <li className="h-14 w-50 text-center rounded-xl bg-white hover:bg-orange-500 hover:text-xl"><b><a href="https://github.com/adithya1770/To-N-Do">To'N'Do</a></b> : A todo app made using ExpressJS</li><br />
-                <li className="h-20 w-50 text-center rounded-xl bg-white hover:bg-orange-500 hover:text-xl"><b><a href="https://github.com/adithya1770/sceptix-webapp">OpenMath</a></b> : A Matrix calculator and Math Website</li><br />
-                <li className="h-14 w-50 text-center rounded-xl bg-white hover:bg-orange-500 hover:text-xl" ><b><a href="https://github.com/adithya1770/learn-react">Farmy</a></b> : Web portal for famers(Beta)</li><br />
-                <li className="h-14 w-50 text-center rounded-xl bg-white hover:bg-orange-500 hover:text-xl"><b className="underline hover:text-orange-500">SaveLyf</b> : Alcohol Detector using RaspberryPI</li>
+                <li className="h-64 w-full text-center rounded-xl bg-white blur hover:blur-none hover:contrast-125 hover:text-xl"><b><a href="https://github.com/adithya1770/To-N-Do">To'N'Do</a></b>  <img src="./assets/cap1.png" className="h-50 w-full"/> A todo app made using ExpressJS</li><br />
+                <li className="h-64 w-full text-center rounded-xl bg-white hover:text-xl blur hover:blur-none hover:contrast-125"><b><a href="https://github.com/adithya1770/sceptix-webapp">OpenMath</a></b> <img src="./assets/Capture.png" className="h-50 w-full"/> A Matrix calculator and Math Website</li><br />
+                <li className="h-64 w-full text-center rounded-xl bg-white hover:text-xl blur hover:blur-none hover:contrast-125" ><b><a href="https://github.com/adithya1770/learn-react">Farmy</a></b>  <img src="./assets/home.png" className="h-50 w-full"/> Web portal for famers(Beta)</li><br />
+                <li className="h-14 w-50 text-center rounded-xl bg-white hover:text-xl blur hover:blur-none hover:contrast-125"><b className="underline pt-10 hover:text-orange-500">SaveLyf</b> : Alcohol Detector using RaspberryPI</li>
             </ul>
         </div>
     )
